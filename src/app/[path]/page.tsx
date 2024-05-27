aa6582d704fda6da57334ff4bc180b0ca34dfbc9
HASURO <rentaro170928@icloud.com>
2024-05-28 01:08:03 +0900

    first commit

--- src/app/[path]/page.tsx
+++ src/app/[path]/page.tsx
@@ -3,11 +3,12 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Home() {
+export default function PathPage({ params }: { params: { path: string } }) {
+  const { path } = params;
   const router = useRouter();
 
   useEffect(() => {
-    router.push("https://github.com/LiT-Kansai-Members-Org");
+    router.push(`https://github.com/LiT-Kansai-Members-Org/${path}`);
   }, []);
 
   return <></>;
